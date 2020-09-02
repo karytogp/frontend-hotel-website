ca52709f53596df424875e5c03f70922c12b303d
Carol G <karytogp@gmail.com>
2020-09-02 18:01:37 -0500

    usando grunt

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function(){
+    $("[data-toggle='tooltip']").tooltip();
+    $("[data-toggle='popover']").popover();
+    $('.carousel').carousel ({
+       interval:2000
+    });
+    $('#contacto').on('show.bs.modal', function(e){
+       console.log('El modal se esta mostrando');
+
+       $('#contactoBtn').removeClass('btn-outline-success');
+       $('#contactoBtn').addClass('btn-primary');
+
+       $('#contactoBtn').prop('disabled', true);
+    });
+
+    $('#contacto').on('shown.bs.modal', function(e){
+       console.log('El modal se mostró');
+    });
+
+    $('#contacto').on('hide.bs.modal', function(e){
+       console.log('El modal se oculta');
+    });
+
+    $('#contacto').on('hidden.bs.modal', function(e){
+       console.log('El modal se ocultó');
+       $('#contactoBtn').prop('disabled', false);
+    });
+ });
